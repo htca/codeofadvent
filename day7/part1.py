7c75688697dcd16d85f8695fc3b84c2a2e735edd
htca <marcel@familiethart.nl>
2023-12-07 11:07:19 +0100

    day 7 cleaned

--- day7/part1.py
+++ day7/part1.py
@@ -10,37 +10,26 @@ with open(text_file,"r") as file:
 replace = {"A":14,"K":13,"Q":12, "J":11, "T":10}
 total_scores =[]
 for line in data:
-    print (line)
     array = line.split()[0]
     cards = [x for x in array]
-    print(cards)
     cards_replaced = [int(x) for x in [replace.get(item, item) for item in cards]]
-    print(cards_replaced)
     bet = int(line.split()[1])
     unique_count = Counter(array)
     type = sorted(unique_count.values())[::-1]
-    print(type)
     type_score = 0
     if type == [5]:
-        print("five of a kind")
         type_score = 7
     elif type == [4,1]:
-        print("four of a kind")
         type_score = 6
     elif type == [3,2]:
-        print("full house")
         type_score = 5
     elif type == [3,1,1]:
-        print("three of a kind")
         type_score = 4
     elif type == [2,2,1]:
-        print("two pair")
         type_score = 3
     elif type == [2,1,1,1]:
-        print("one pair")
         type_score = 2
     elif type == [1,1,1,1,1]:
-        print("High card")
         type_score = 1
     else:
         print("error")
@@ -49,12 +38,9 @@ for line in data:
         scorecard.append(c)
     scorecard.append(bet)
     total_scores.append(scorecard)
-print(np.array(total_scores))
 sorted_array = np.array(sorted(total_scores, key=lambda x: (x[0], x[1], x[2], x[3], x[4], x[5])))
-print(sorted_array)
 ranked_array = [(rank + 1, row) for rank, row in enumerate(sorted_array)]
-print(ranked_array)
 totalscore = 0
 for row in ranked_array:
     totalscore += row[0]*row[1][-1]
-print(totalscore)
+print("part 1:",totalscore)
